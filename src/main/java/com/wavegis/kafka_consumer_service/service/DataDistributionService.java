8d63f2fc1449c18a98eb3116a7a6001407323423
KodaKo-WaveGIS <koda_ko@wavegis.com.tw>
2024-07-03 10:21:56 +0800

    2024.07.03-增加多執行續併行處理

--- src/main/java/com/wavegis/kafka_consumer_service/service/DataDistributionService.java
+++ src/main/java/com/wavegis/kafka_consumer_service/service/DataDistributionService.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Function;
+
+import javax.annotation.PostConstruct;
 
 import org.eclipse.jetty.server.RequestLog.Collection;
 import org.slf4j.Logger;
@@ -40,43 +45,57 @@ public class DataDistributionService {
     
     private Function<String, Boolean> isTpeswerHasStnos = st_no->tpeSewerService.isTpeswerHasStnos(st_no);
     
-    public void distribution(String topices, String st_no, String kafkaMessage) {
+    private Function<String, KafkaDTO> prepareDto;
+    
+    @PostConstruct
+    private void init() {
         
-        List<IowPublisherPostVO> iowPublisherPostVoList;
-        List<NtouPublisherPostVO> ntouPublisherPostVoList;
-        KafkaDTO dto;
-        String[] strs;
-        int resCode = 202;
-        
-        if(iowSensorDtoMap.containsKey(st_no)) {
-            dto = new KafkaDTO();
-            strs = kafkaMessage.split(",");
+        // 將資料處理和 API 呼叫抽成方法，減少重複碼
+        prepareDto = (kafkaMessage) -> {
+            KafkaDTO dto = new KafkaDTO();
+            String[] strs = kafkaMessage.split(",");
             dto.setStrs(strs);
-            
-            iowPublisherPostVoList = new ArrayList<IowPublisherPostVO>();
-            iowPublisherPostVoList.add(Util.toVo(dto, new IowPublisherPostVO()));
-            resCode = iowPublisherApiService.postData(st_no, iowPublisherPostVoList);
-            logger.info("Iow---topics={}, resCode={}, st_no={}, water_inner={}, datatime={}",topices, resCode, st_no, dto.getWater_inner(), dto.getDatatime());
-        }
+            return dto;
+        };
+    }
+    
+    public void distribution(String topices, String st_no, String kafka_message) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
         
-        if(ntouSensorDtoMap.containsKey(st_no.toLowerCase())) {
-            dto = new KafkaDTO();
-            strs = kafkaMessage.split(",");
-            dto.setStrs(strs);
-            
-            ntouPublisherPostVoList = new ArrayList<NtouPublisherPostVO>();
-            ntouPublisherPostVoList.add(Util.toVo(dto, new NtouPublisherPostVO()));
-            resCode = ntouPublisherApiService.postData(st_no, ntouPublisherPostVoList);
-            logger.info("Ntou---topics={}, resCode={}, st_no={}, water_inner={}, datatime={}",topices, resCode, st_no, dto.getWater_inner(), dto.getDatatime());
-        }
+        CompletableFuture<Void> futureIow = CompletableFuture.runAsync(() -> {
+            if(iowSensorDtoMap.containsKey(st_no)) {
+                KafkaDTO dto = prepareDto.apply(kafka_message);
+                List<IowPublisherPostVO> iowPublisherPostVoList = new ArrayList<IowPublisherPostVO>();
+                iowPublisherPostVoList.add(Util.toVo(dto, new IowPublisherPostVO()));
+                int resCode = iowPublisherApiService.postData(st_no, iowPublisherPostVoList);
+                logger.info("Iow---topics={}, resCode={}, st_no={}, water_inner={}, datatime={}",topices, resCode, st_no, dto.getWater_inner(), dto.getDatatime());
+            }
+        },executor);
         
-        if(isTpeswerHasStnos.apply(st_no)) {
-            dto = new KafkaDTO();
-            strs = kafkaMessage.split(",");
-            dto.setStrs(strs);
-            
-            resCode = tpeSewerService.postData(st_no, Collections.singletonList(new TpeSewerPublisherPostVO().fromKafkaDTO(dto)));
-            logger.info("Tpesewer---topics={}, resCode={}, st_no={}, water_inner={}, datatime={}",topices, resCode, st_no, dto.getWater_inner(), dto.getDatatime());
-        }
+        CompletableFuture<Void> futureNtou = CompletableFuture.runAsync(() -> {
+            if(ntouSensorDtoMap.containsKey(st_no.toLowerCase())) {
+                KafkaDTO dto = prepareDto.apply(kafka_message);
+                List<NtouPublisherPostVO> ntouPublisherPostVoList = new ArrayList<NtouPublisherPostVO>();
+                ntouPublisherPostVoList.add(Util.toVo(dto, new NtouPublisherPostVO()));
+                int resCode = ntouPublisherApiService.postData(st_no, ntouPublisherPostVoList);
+                logger.info("Ntou---topics={}, resCode={}, st_no={}, water_inner={}, datatime={}",topices, resCode, st_no, dto.getWater_inner(), dto.getDatatime());
+            }
+        },executor);
+        
+        CompletableFuture<Void> futureTpesewer = CompletableFuture.runAsync(() -> {
+            if(isTpeswerHasStnos.apply(st_no)) {
+                KafkaDTO dto = prepareDto.apply(kafka_message);
+                int resCode = tpeSewerService.postData(st_no, Collections.singletonList(new TpeSewerPublisherPostVO().fromKafkaDTO(dto)));
+                logger.info("Tpesewer---topics={}, resCode={}, st_no={}, water_inner={}, datatime={}",topices, resCode, st_no, dto.getWater_inner(), dto.getDatatime());
+            }
+        },executor);
+        
+
+        CompletableFuture.allOf(futureIow, futureNtou, futureTpesewer)
+        .thenRun(() -> logger.info("All conditions completed."))
+        .join();
+
+        executor.shutdown();
     }
 }
